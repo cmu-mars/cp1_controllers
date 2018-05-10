b245d3efd06deada49807f9abb79bf8b4a330c21
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2018-05-10 07:53:45 -0500

    fixed tab error

--- robotcontrol/rainbow_interface.py
+++ robotcontrol/rainbow_interface.py
@@ -66,7 +66,7 @@ class RainbowInterface:
         print ("Configuring rainbow for %s"%challenge_problem)
         os.environ["LD_LIBRARY_PATH"] = os.environ["LD_LIBRARY_PATH"] + ":" + os.path.expanduser("~/das/prism-4.3.1-linux64/lib")
         print ("LD_LIBRARY_PATH=%s" %os.environ["LD_LIBRARY_PATH"])
-	rainbow_sleep = os.environ["RAINBOW_WAIT_TIME"]
+	    rainbow_sleep = os.environ["RAINBOW_WAIT_TIME"]
         self.target = self.getTarget(challenge_problem)
         if (self.target is not None):
             time.sleep(10)
